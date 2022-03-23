3c0060ceb69265026cb0bf7cce63051c8cbcc9dc
Luis Antonio Linares Carbajal <l171070100@milpaalta.tecnm.mx>
2022-03-22 22:27:33 -0600

    interfaces

---
+++ src/Interface/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author Tony
+ */
+public class Main {
+    public static void main(String []args){
+        lenguaje lenguaje= new lenguaje();
+        programaLenguaje pl= new programaLenguaje();
+        
+        lenguaje.sayhi();
+        pl.sayhi();
+    }
+}
---
+++ src/Interface/lenguaje.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author Tony
+ */
+public class lenguaje implements NewInterface{
+
+    @Override
+    public void sayhi(){
+        System.out.println("Hola");
+    }
+    
+}
